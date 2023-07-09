884aa81fb342624df42f77b6e31fbc045475ba0f
Phil Stevens <email@philstevens.com>
2023-07-09 15:51:34 -0400

    Changed version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='fancywallet', 
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
 #    email='gmartins@fancywhale.ca',
 #    author='Gui Martins',
